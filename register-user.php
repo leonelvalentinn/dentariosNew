6af767927321f197272eba5a87bd464e803d2cdb
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-16 17:23:24 -0600

    Add notification to exito

--- register-user.php
+++ register-user.php
@@ -42,7 +42,7 @@ if ($stmt = $conn->prepare('INSERT INTO clients (name, username, password, email
   // Execute the query using the data we just defined
   // The execute() method returns TRUE if it is successful and FALSE if it is not, allowing you to write your own messages here
   if ($stmt->execute()) {
-    header('Location: login.php');
+    header('Location: login.php?exito=1');
   } else {
     echo "Unable to create record";
   }
