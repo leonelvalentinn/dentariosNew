85a0181c058380711ad5a55a87278e246f420552
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-15 21:06:17 -0600

    Change Header to Perfil

--- authentication.php
+++ authentication.php
@@ -54,7 +54,7 @@ if ($stmt->num_rows > 0) {
         $_SESSION['loggedin'] = TRUE;
         $_SESSION['name'] = $_POST['username'];
         $_SESSION['id'] = $id;
-        header('Location: nosotros.php');
+        header('Location: perfil.php');
     }
 } else {
 
